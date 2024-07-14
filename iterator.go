8c082b6d4ca691c11ad36bf3895e27047f3b2649
Brian Green <bgreen007@cox.net>
2024-07-14 06:53:03 -0500

    iterator tweaks

--- iterator.go
+++ iterator.go
@@ -23,20 +23,15 @@ func (si *SimpleIteratorImpl) HasNext() bool {
 	return si.hasNext
 }
 
-// func (si *SimpleIteratorImpl) readBytes(numBytes int32) []byte {
-
-// }
-
 func (si *SimpleIteratorImpl) Next() interface{} {
 
 	// use si.returnType to create a new instance of the same type
 	newItem := reflect.New(si.returnType)
 	loggable := newItem.Interface().(BinaryLoggable)
 
-	//
+	// Get the size of the log entry
 	rval, _, err := si.reader.ReadRune()
 	if err != nil {
-		// check if EOF:
 		if errors.Is(err, io.EOF) {
 			si.hasNext = false
 			return nil
@@ -49,21 +44,21 @@ func (si *SimpleIteratorImpl) Next() interface{} {
 	buf := make([]byte, bytesToRead)
 	nBytes, err := si.reader.Read(buf)
 	if err != nil {
-		log.Fatalln("failed to read from reader:", err)
 		si.hasNext = false
+		log.Fatalln("failed to read from reader:", err)
 		return nil
 	}
 
-	// its possible that the current block doesnt have all the data:
-	// if this happens, attempt to read the missing bytes, which should trigger bufio to read
-	// the next block from file
+	// In the event that the current block doesnt have all the data,
+	// repeat the read, which should trigger the reader to load the next block from disk
+	// retry n times
+	// TODO add retry logic
 	if nBytes != bytesToRead {
 		missingBytes := bytesToRead - nBytes
 		//fmt.Println("didnt read enough bytes - missing:", missingBytes)
 		tbytesRead, err := si.reader.Read(buf[nBytes:])
 		if err != nil {
-			// TODO better error handling here
-			//
+			log.Fatalln("failed to read from reader:", err)
 		}
 		if tbytesRead != missingBytes {
 			log.Println("still missing bytes: read:", tbytesRead, "needed:", missingBytes)
